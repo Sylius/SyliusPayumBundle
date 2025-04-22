63d32debddec3faa153264bb1ad9f063babde909
Francis Hilaire <Prometee@users.noreply.github.com>
2025-04-22 14:59:34 +0200

    Import names

--- Tests/DependencyInjection/Compiler/ConditionalGatewayConfigEncryptionCheckerDecoratorPassTest.php
+++ Tests/DependencyInjection/Compiler/ConditionalGatewayConfigEncryptionCheckerDecoratorPassTest.php
@@ -13,6 +13,7 @@ declare(strict_types=1);
 
 namespace Sylius\Bundle\PayumBundle\Tests\DependencyInjection\Compiler;
 
+use PHPUnit\Framework\Attributes\Test;
 use Matthias\SymfonyDependencyInjectionTest\PhpUnit\AbstractCompilerPassTestCase;
 use Sylius\Bundle\PayumBundle\DependencyInjection\Compiler\ConditionalGatewayConfigEncryptionCheckerDecoratorPass;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
@@ -21,7 +22,7 @@ use Symfony\Component\DependencyInjection\Reference;
 
 final class ConditionalGatewayConfigEncryptionCheckerDecoratorPassTest extends AbstractCompilerPassTestCase
 {
-    #[\PHPUnit\Framework\Attributes\Test]
+    #[Test]
     public function it_registers_gateway_config_encryption_checker_decorator_if_checker_service_exists(): void
     {
         $this->setDefinition('sylius.checker.gateway_config_encryption', new Definition());
@@ -31,7 +32,7 @@ final class ConditionalGatewayConfigEncryptionCheckerDecoratorPassTest extends A
         $this->assertContainerBuilderHasServiceDefinitionWithArgument('sylius_payum.checker.gateway_config_encryption', 0, new Reference('.inner'));
     }
 
-    #[\PHPUnit\Framework\Attributes\Test]
+    #[Test]
     public function it_does_not_register_gateway_config_encryption_checker_decorator_if_checker_service_does_not_exist(): void
     {
         $this->compile();
